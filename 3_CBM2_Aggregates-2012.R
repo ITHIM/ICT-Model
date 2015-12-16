34487e597fa9d9fc0dbc8f34c9169e3c7c643b49
usr110 <m.aliabbas@gmail.com>
2015-12-16 11:06:38 +0000

    Included people without trips with two mmets. Also updated the code to reflect scenarios as R objects (not csv files)

--- 3_CBM2_Aggregates-2012.R
+++ 3_CBM2_Aggregates-2012.R
@@ -1,5 +1,5 @@
 #setwd("//me-filer1/home$/au232/My Documents/1.CEDAR/3_Studies !!/23-CBM2/3-Code")
-rm(list=ls())
+#rm(list=ls())
 source('AggCalc.R')   
 
 library(dplyr)
@@ -11,7 +11,7 @@ library(sqldf)
 #setwd("V:/Studies/MOVED/HealthImpact/CBM2/Code/Scenarios2012_England") #source folder
 
 #read baseline & add short walks
-baseline <-read.csv('baseline.csv',header=T,as.is=T)
+baseline <- read.csv('bl2012_18_84ag_sw.csv', header=T, as.is = T)
 
 # #add shortwalks
 # dfsw <-baseline[baseline$MainMode_B03ID==1,]
@@ -19,56 +19,53 @@ baseline <-read.csv('baseline.csv',header=T,as.is=T)
 # 
 # for (i in 1:6) {shortwalks <-rbind(shortwalks,dfsw) }
 # baseline <- rbind(baseline,shortwalks)    #short walks added
-# baseline <- baseline[order(baseline$IndividualID),]     #re-ordering by individuals
+# baseline <- baseline[order(baseline$ID),]     #re-ordering by individuals
 # rm(shortwalks,dfsw)
 
-fnotrips  <- read.csv('People_w_NoTrips2012_ENG_v5.csv',header=T,as.is=T)
+fnotrips  <- read.csv('People_w_NoTrips2012_ENG_v6_anon.csv',header=T,as.is=T)
 
-#DF for results
-df <- data.frame()
-
-todos<-list.files(pattern="[0-9].csv")
-todos <-c('baseline.csv',todos)
-nfiles <-length(todos)
+# todos<-list.files(pattern="[0-9].csv")
+# todos <-c('baseline.csv',todos)
+# nfiles <-length(todos)
 
 
 #Individuals figures
-nopeople <-length(unique(baseline$IndividualID))
-no.males  <- length(unique(baseline$IndividualID[baseline$Sex_B01ID==1]))
-no.females  <-length(unique(baseline$IndividualID[baseline$Sex_B01ID==2]))     
+nopeople <-length(unique(baseline$ID))
+no.males  <- length(unique(baseline$ID[baseline$Sex_B01ID==1]))
+no.females  <-length(unique(baseline$ID[baseline$Sex_B01ID==2]))     
 
-no.white <- length(unique(baseline$IndividualID[baseline$EthGroupTS_B02ID==1]))
-no.nonwhite <- length(unique(baseline$IndividualID[baseline$EthGroupTS_B02ID==2]))
+no.white <- length(unique(baseline$ID[baseline$EthGroupTS_B02ID==1]))
+no.nonwhite <- length(unique(baseline$ID[baseline$EthGroupTS_B02ID==2]))
 
-no.caraccess <- length(unique(baseline$IndividualID[baseline$CarAccess_B01ID %in% c(1,2,3,4)]))  #check
-no.noncaraccess <- length(unique(baseline$IndividualID[baseline$CarAccess_B01ID %in% c(5,6)]))  #check
+no.caraccess <- length(unique(baseline$ID[baseline$CarAccess_B01ID %in% c(1,2,3,4)]))  #check
+no.noncaraccess <- length(unique(baseline$ID[baseline$CarAccess_B01ID %in% c(5,6)]))  #check
 
-no.nssec1 <- length(unique(baseline$IndividualID[baseline$NSSec_B03ID==1]))
-no.nssec2 <- length(unique(baseline$IndividualID[baseline$NSSec_B03ID==2]))
-no.nssec3 <- length(unique(baseline$IndividualID[baseline$NSSec_B03ID==3]))
-no.nssec4 <- length(unique(baseline$IndividualID[baseline$NSSec_B03ID==4]))
-no.nssec5 <- length(unique(baseline$IndividualID[baseline$NSSec_B03ID==5]))
+no.nssec1 <- length(unique(baseline$ID[baseline$NSSec_B03ID==1]))
+no.nssec2 <- length(unique(baseline$ID[baseline$NSSec_B03ID==2]))
+no.nssec3 <- length(unique(baseline$ID[baseline$NSSec_B03ID==3]))
+no.nssec4 <- length(unique(baseline$ID[baseline$NSSec_B03ID==4]))
+no.nssec5 <- length(unique(baseline$ID[baseline$NSSec_B03ID==5]))
 
 #Individuals with no trips
-notripspeople <-length(unique(fnotrips$IndividualID))
-no.males.wotrips <- length(unique(fnotrips$IndividualID[fnotrips$Sex_B01ID==1]))
-no.females.wotrips <- length(unique(fnotrips$IndividualID[fnotrips$Sex_B01ID==2]))
+notripspeople <-length(unique(fnotrips$ID))
+no.males.wotrips <- length(unique(fnotrips$ID[fnotrips$Sex_B01ID==1]))
+no.females.wotrips <- length(unique(fnotrips$ID[fnotrips$Sex_B01ID==2]))
 
-no.white.wotrips <- length(unique(fnotrips$IndividualID[fnotrips$EthGroupTS_B02ID==1]))
-no.nonwhite.wotrips <- length(unique(fnotrips$IndividualID[fnotrips$EthGroupTS_B02ID==2]))
+no.white.wotrips <- length(unique(fnotrips$ID[fnotrips$EthGroupTS_B02ID==1]))
+no.nonwhite.wotrips <- length(unique(fnotrips$ID[fnotrips$EthGroupTS_B02ID==2]))
 
-no.caraccess.wotrips <- length(unique(fnotrips$IndividualID[fnotrips$CarAccess_B01ID %in% c(1,2,3,4)]))
-no.noncaraccess.wotrips <- length(unique(fnotrips$IndividualID[fnotrips$CarAccess_B01ID %in% c(5,6)]))
+no.caraccess.wotrips <- length(unique(fnotrips$ID[fnotrips$CarAccess_B01ID %in% c(1,2,3,4)]))
+no.noncaraccess.wotrips <- length(unique(fnotrips$ID[fnotrips$CarAccess_B01ID %in% c(5,6)]))
 
-no.nssec1.wotrips <- length(unique(fnotrips$IndividualID[fnotrips$NSSec_B03ID==1]))
-no.nssec2.wotrips <- length(unique(fnotrips$IndividualID[fnotrips$NSSec_B03ID==2]))
-no.nssec3.wotrips <- length(unique(fnotrips$IndividualID[fnotrips$NSSec_B03ID==3]))
-no.nssec4.wotrips <- length(unique(fnotrips$IndividualID[fnotrips$NSSec_B03ID==4]))
-no.nssec5.wotrips <- length(unique(fnotrips$IndividualID[fnotrips$NSSec_B03ID==5]))
+no.nssec1.wotrips <- length(unique(fnotrips$ID[fnotrips$NSSec_B03ID==1]))
+no.nssec2.wotrips <- length(unique(fnotrips$ID[fnotrips$NSSec_B03ID==2]))
+no.nssec3.wotrips <- length(unique(fnotrips$ID[fnotrips$NSSec_B03ID==3]))
+no.nssec4.wotrips <- length(unique(fnotrips$ID[fnotrips$NSSec_B03ID==4]))
+no.nssec5.wotrips <- length(unique(fnotrips$ID[fnotrips$NSSec_B03ID==5]))
 
 #cyclists
-ncyclists0 <-length(unique(baseline$IndividualID[baseline$Cycled==1]))
-cyclists0.perc <-round(100* ncyclists0 /(length(unique(baseline$IndividualID))+notripspeople),1)
+ncyclists0 <-length(unique(baseline$ID[baseline$Cycled==1]))
+cyclists0.perc <-round(100* ncyclists0 /(length(unique(baseline$ID))+notripspeople),1)
 
 # baseline nos.
 carMiles0 <-sum(baseline[baseline$MainMode_B04ID %in% c(3,4,5,12),'TripDisIncSW'])
@@ -83,13 +80,26 @@ CO20 <- round(carMiles0 * 1.61 * (3.1 / 1.61) * 1e-4,2)   #(in metric Tons)
 
 TripTotalTime0 <- round(sum(baseline$TripTotalTime),1)
 
-for (i1 in (1:nfiles)) {  #reading files for aggregates
-  #for (i1 in (1:3)) {
-  
+#DF for results
+df <- AggCalc(baseline, "baseline")
+
+for (i1 in 1:length(listOfScenarios)) {  #reading files for aggregates
+
   #DATA AGGREGATES
-  AggCalc(todos[i1])
+  tbl <- baseline
+  sc <- get(as.character(listOfScenarios[i1]) )
+  tbl$now_cycle <- sc$now_cycle
+  tbl$ebike <- sc$ebike
+  tbl$cyclist <- sc$cyclist
+  tbl$METh <- sc$METh
+  tbl$MMETh <- sc$MMETh
+  tbl$TripTotalTime1 <- sc$TripTotalTime1
+#   "now_cycle"      "ebike"          "cyclist"        "METh"           "MMETh"         
+#   "TripTotalTime1"
   
-  ifelse(i1==1,df <-info,df<-rbind(df,info))   #consolidates content
+  info <- AggCalc(tbl, as.character(listOfScenarios[i1]))
+  
+  df <- rbind(df,info)   #consolidates content
   
   if ((i1%%1) == 0) {
     message('no files: ',i1)
