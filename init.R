9c8ec5994f0863155ee4a5ad2e80318d41e0adc0
usr110 <m.aliabbas@gmail.com>
2016-01-13 11:06:58 +0000

    Update list of scenarios with the reduced number of scenarios.

--- init.R
+++ init.R
@@ -1,5 +1,10 @@
 # Initialize the state of the project
-rm(list= ls()[!(ls() %in% listOfScenarios)])
-listOfScenarios <- as.list(read.csv("listofScenarios.csv", header = F, as.is = T))
+#rm(list= ls()[!(ls() %in% listOfScenarios)])
+#listOfScenarios <- c("MS1_ebik0_eq0", "MS1_ebik0_eq1", "MS1_ebik1_eq0", "MS1_ebik1_eq1")
+
+#listOfScenarios <- as.list(read.csv("listofScenarios.csv", header = F, as.is = T))
+# Read MS1 Excluding Scenario list
+listOfScenarios <- as.list(read.csv("listofScenariosExcMS1.csv", header = F, as.is = T))
+
 people_with_no_trips <- read.csv("People_w_NoTrips2012_ENG_v6_anon.csv", header = T, as.is = T)
 mmet2RR_mat <- read.csv("health calculations/Wen et al all cause mortality RRs.csv", header = T, as.is = T)
