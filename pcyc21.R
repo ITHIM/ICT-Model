f446e89d45dfa2810462e58bbc2fe16bae5cf102
usr110 <m.aliabbas@gmail.com>
2016-06-28 16:22:41 +0100

    Set initial prob to zero

--- pcyc21.R
+++ pcyc21.R
@@ -5,7 +5,7 @@ pcyc21 <-function(age,sex,dist,ebikes,equity,MS) {
   distIntervals <-c(0.5,1.5,2.5,3.5,4.5,5.5,6.5,9.5,12.5,15.5,20.5,30.5,40.5,10000)
   #get interval to use as nrow
   nrow <- findInterval(dist,distIntervals)+1  # starts @ 0, add 1
-  
+  prob <- 0
   if (ebikes == 0) {
     ncol <- paste(sex,age,sep="_")
     prob <- pcycl_baseline[nrow,ncol]   
