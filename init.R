7a1d33585f0c188fd480395f2390af9058eebf3b
usr110 <m.aliabbas@gmail.com>
2016-07-22 14:25:31 +0100

    Read listOfScenarios if it doesn't exist as an R object

--- init.R
+++ init.R
@@ -1,10 +1,12 @@
 # Initialize the state of the project
-#rm(list= ls()[!(ls() %in% listOfScenarios)])
-#listOfScenarios <- c("MS1_ebik0_eq0", "MS1_ebik0_eq1", "MS1_ebik1_eq0", "MS1_ebik1_eq1")
 
-#listOfScenarios <- as.list(read.csv("listofScenarios.csv", header = F, as.is = T))
-# Read MS1 Excluding Scenario list
-listOfScenarios <- as.list(read.csv("listofScenariosExcMS1.csv", header = F, as.is = T))
+# Check if listOfScenarios exists. If not, then read it from a csv file
+if (!exists("listOfScenarios")){
+  listOfScenarios <- read.csv("listofScenarios.csv", header = F, as.is = T)
+  listOfScenarios <- as.list(listOfScenarios$V1)
+}
 
-people_with_no_trips <- read.csv("People_w_NoTrips2012_ENG_v6_anon.csv", header = T, as.is = T)
+# No need to read people with no trips
+#people_with_no_trips <- read.csv("People_w_NoTrips2012_ENG_v6_anon.csv", header = T, as.is = T)
+
 mmet2RR_mat <- read.csv("health calculations/Wen et al all cause mortality RRs.csv", header = T, as.is = T)
