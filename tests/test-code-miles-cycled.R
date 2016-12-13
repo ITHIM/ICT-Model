844962f2c086097e2f851f13437aa0411b145904
usr110 <m.aliabbas@gmail.com>
2016-12-13 13:29:53 +0000

    Fix variable names.

--- tests/test-code-miles-cycled.R
+++ tests/test-code-miles-cycled.R
@@ -3,8 +3,8 @@
 # Subset milescycled for England
 mc_region <- subset(milesCycled.pers, HHoldGOR_B02ID == 0)
 # Subset columns for baseline and the first scenario 'MS0.05_ebik0_eq0'
-mc_two_col <- td1 <- subset(td, select = c(baseline, MS0.05_ebik0_eq0))
+mc_two_col <- subset(td, select = c(baseline, MS0.05_ebik0_eq0))
 # Create a table with bins for baseline
-bltb <- as.data.frame(table (cut (td1$baseline, breaks = c(c(-1, 0, 2, 5, 10, 20, 40, 60), max(td1$baseline)))))
+bltb <- as.data.frame(table (cut (mc_two_col$baseline, breaks = c(c(-1, 0, 2, 5, 10, 20, 40, 60), max(mc_two_col$baseline)))))
 # Create a table with bins for scenario
-sctb <- as.data.frame(table (cut (td1$MS0.05_ebik0_eq0, breaks = c(c(-1, 0, 2, 5, 10, 20, 40, 60), max(td1$MS0.05_ebik0_eq0)))))
+sctb <- as.data.frame(table (cut (mc_two_col$MS0.05_ebik0_eq0, breaks = c(c(-1, 0, 2, 5, 10, 20, 40, 60), max(mc_two_col$MS0.05_ebik0_eq0)))))
