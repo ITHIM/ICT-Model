df816d6516191bee4c98546aca025a42ab4d68a4
usr110 <m.aliabbas@gmail.com>
2017-11-23 17:16:10 +0000

    Re-add init script and correct mortality data's path

---
+++ init.R
@@ -0,0 +1,12 @@
+# Initialize the state of the project
+
+# Check if listOfScenarios exists. If not, then read it from a csv file
+if (!exists("listOfScenarios") || length(listOfScenarios) != 24){
+  listOfScenarios <- read.csv("listofScenarios.csv", header = F, as.is = T)
+  listOfScenarios <- as.list(listOfScenarios$V1)
+}
+
+# No need to read people with no trips
+#people_with_no_trips <- read.csv("People_w_NoTrips2012_ENG_v6_anon.csv", header = T, as.is = T)
+
+mmet2RR_mat <- read.csv("healthcalculations/MA-all-cause-mortality-RRs.csv", header = T, as.is = T)
